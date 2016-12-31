14571a3c9686036dc25d05ed28785bce50d3084d
mdymczyk <dymczyk@gmail.com>
2017-01-01 01:55:29 +0900

    Adds pushing to prediction queue

--- predictions/src/main/scala/Predictor.scala
+++ predictions/src/main/scala/Predictor.scala
@@ -1,9 +1,10 @@
-import java.io.IOException
+import java.util.Properties
 
 import hex.genmodel.GenModel
 import hex.genmodel.easy.prediction.BinomialModelPrediction
 import hex.genmodel.easy.{EasyPredictModelWrapper, RowData}
 import org.apache.kafka.clients.consumer.{ConsumerRecord, ConsumerRecords, OffsetAndMetadata}
+import org.apache.kafka.clients.producer.{KafkaProducer, Producer, ProducerConfig, ProducerRecord}
 import org.apache.kafka.common.TopicPartition
 
 import scala.collection.mutable
@@ -17,12 +18,31 @@ object Predictor {
   val modelClassName = "autoencoder"
   val sensorTopic = "sensor1"
   val predictionTopic = "sensor-state-test"
+  val producer: KafkaProducer[String, String] = makeProducer()
+
+  private def makeProducer() = {
+    val props = new Properties()
+
+    // TODO check properties
+    props.put("bootstrap.servers", "localhost:9092")
+    props.put("acks", "all")
+    props.put("retries", 0)
+    props.put("batch.size", 16384)
+    props.put("linger.ms", 1)
+    props.put("buffer.memory", 33554432)
+    props.put("key.serializer", "org.apache.kafka.common.serializa-tion.StringSerializer")
+    props.put("value.serializer", "org.apache.kafka.common.serializa-tion.StringSerializer")
+
+    new KafkaProducer[String, String](props)
+  }
 
   def main(args: Array[String]): Unit = {
     val consumer = new MapRStreamsConsumerFacade(sensorTopic)
     consumer.prepareSetup()
     consumer.open()
     poll(consumer)
+    producer.close()
+    consumer.close()
   }
 
   private def record2row(s: String) = {
@@ -32,7 +52,14 @@ object Predictor {
     row
   }
 
-  private def pushPrediction(label: String) = ???
+  private def pushPrediction(label: String) = {
+    producer.send(
+      new ProducerRecord[String, String](
+        predictionTopic,
+        "state",
+        label)
+    )
+  }
 
   def poll(consumer: MapRStreamsConsumerFacade): Unit = {
     val rawModel: GenModel = Class.forName(modelClassName).newInstance().asInstanceOf[GenModel]
