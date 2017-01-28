9be9a95b0b4076ea0e6ae66fcda42ab36288a086
mdymczyk <dymczyk@gmail.com>
2017-01-28 19:59:28 +0900

    Adds polling from the sensor queue and predicting values

--- predictions/build.sbt
+++ predictions/build.sbt
@@ -4,4 +4,8 @@ scalaVersion := "2.11.8"
 
 resolvers += Resolver.bintrayRepo("cakesolutions", "maven")
 
-libraryDependencies += "org.apache.kafka" % "kafka-clients" % "0.10.1.0"
+libraryDependencies ++= Seq(
+  "org.apache.kafka" % "kafka-clients" % "0.10.1.0",
+  "ai.h2o" % "h2o-genmodel" % "3.10.2.2",
+  "org.scala-lang.modules" %% "scala-parser-combinators" % "1.0.4"
+)
--- predictions/src/main/scala/Predictor.scala
+++ predictions/src/main/scala/Predictor.scala
@@ -1,29 +1,58 @@
 import java.io.IOException
 
+import hex.genmodel.GenModel
+import hex.genmodel.easy.prediction.BinomialModelPrediction
+import hex.genmodel.easy.{EasyPredictModelWrapper, RowData}
 import org.apache.kafka.clients.consumer.{ConsumerRecord, ConsumerRecords, OffsetAndMetadata}
 import org.apache.kafka.common.TopicPartition
 
 import scala.collection.mutable
+import scala.util.parsing.json.JSON
 
 /**
   * Created by mateusz on 2017/01/26.
   */
 object Predictor {
 
+  val modelClassName = "autoencoder"
+  val sensorTopic = "sensor1"
+  val predictionTopic = "sensor-state-test"
+
   def main(args: Array[String]): Unit = {
-    val consumer = new MapRStreamsConsumerFacade("sensor-state-test")
+    val consumer = new MapRStreamsConsumerFacade(sensorTopic)
     consumer.prepareSetup()
     consumer.open()
     poll(consumer)
   }
 
+  private def record2row(s: String) = {
+    val row: RowData = new RowData()
+    val jsonRecord = JSON.parseFull(s).get.asInstanceOf[Map[String, AnyRef]]
+    jsonRecord.foreach{ case (key, value) => row.put(key, value)}
+    row
+  }
+
+  private def pushPrediction(label: String) = ???
+
   def poll(consumer: MapRStreamsConsumerFacade): Unit = {
-    val commitMap = new mutable.LinkedHashMap[TopicPartition, OffsetAndMetadata]();
+    val rawModel: GenModel = Class.forName(modelClassName).newInstance().asInstanceOf[GenModel]
+    val model: EasyPredictModelWrapper = new EasyPredictModelWrapper(rawModel)
 
-    val records: ConsumerRecords[String, String] = consumer.poll()
+    while(true) {
+      val commitMap = new mutable.LinkedHashMap[TopicPartition, OffsetAndMetadata]()
 
-    if (commitMap.nonEmpty) {
-      consumer.commit(commitMap.toMap.toJava)
+      val records: ConsumerRecords[String, String] = consumer.poll()
+
+      for(record: ConsumerRecord[String, String] <- records) {
+        val rowData: RowData = record2row(record.value())
+        val pred: BinomialModelPrediction = model.predictBinomial(rowData)
+        pushPrediction(pred.label)
+      }
+
+      if (commitMap.nonEmpty) {
+        import scala.collection.JavaConversions._
+        consumer.commit(commitMap.toMap)
+      }
     }
   }
 
