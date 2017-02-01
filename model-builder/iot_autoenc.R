653f392ef5ec0726857c2022a833a40f851a7860
mdymczyk <dymczyk@gmail.com>
2017-02-01 14:01:31 +0900

    Performance improvemnts

--- model-builder/iot_autoenc.R
+++ model-builder/iot_autoenc.R
@@ -6,25 +6,52 @@ ngram <- function(inp, window){
     cols <- dim(inp)[2]
     resRows <- rows - window + 1
 
-    res <- c()
-
+    res <- matrix(, nrow = resRows, ncol = window*cols)
     for(idx in 1:resRows) {
-        newRow <- inp[idx,]
-        for(ii in 1:(window-1)) {
-            newRow <- c(newRow, inp[idx+ii,])
+        if(window-1 > 0) {
+            newRow <- c(inp[idx,], t(inp[(idx+1):(idx+window-1),]))
+        } else {
+            newRow <- inp[idx,]
         }
-        res <- rbind(res,newRow)
+        if(idx %% 10000 == 0) {
+            print(idx)
+        }
+        res[idx,] <- t(newRow)
     }
     return(res)
 }
 
+window <- 1
+
 iotRaw <- read.csv("resources/state_0_loop_0.csv")
-iot <- iotRaw[,colSums(iotRaw != 0) > 0]
-iot <- iot[,-c(0,1,2,3)]
+iot <- as.matrix(iotRaw[,c("LinAccX..g.","LinAccY..g.","LinAccZ..g.")])
+#iot <- iotRaw[,colSums(iotRaw != 0) > 0]
+#iot <- as.matrix(iot[,-c(0,1,2,3)])
+
+iot <- ngram(iot, window)
 iot.hex  <- as.h2o(iot)
-iot.dl = h2o.deeplearning(training_frame = iot.hex, autoencoder = TRUE, hidden = c(50, 50, 50), epochs = 100,seed=1)
+
+neurons <- 50
+iot.dl = h2o.deeplearning(x = 1:(ncol(iot)), training_frame = iot.hex, autoencoder = TRUE, hidden = c(neurons), epochs = 100,
+l1 = 1e-5, l2 = 1e-5, max_w2 = 10, activation = "TanhWithDropout", initial_weight_distribution = "UniformAdaptive", adaptive_rate = TRUE)
+iot_error <- h2o.anomaly(iot.dl, iot.hex)
+avg_iot_error <- sum(iot_error)/nrow(iot_error)
+print(nrow(iot_error[iot_error > avg_iot_error])/nrow(iot.hex))
 
 anomalyRaw <- read.csv("resources/state_1_loop_1.csv")
-anomaly <- anomalyRaw[,colSums(anomalyRaw != 0) > 0]
-anomaly <- anomaly[,-c(0,1,2,3)]
+anomaly <- as.matrix(anomalyRaw[,c("LinAccX..g.","LinAccY..g.","LinAccZ..g.")])
+#anomaly <- anomalyRaw[,colSums(anomalyRaw != 0) > 0]
+#anomaly <- as.matrix(anomaly[,-c(0,1,2,3)])
+anomaly <- ngram(anomaly, window)
 anomaly.hex  <- as.h2o(anomaly)
+anomaly_error <- h2o.anomaly(iot.dl, anomaly.hex)
+print(nrow(anomaly_error[anomaly_error > avg_iot_error])/nrow(anomaly.hex))
+
+
+verifyRaw <- read.csv("resources/verify_0.csv")
+verify <- verifyRaw[,colSums(verifyRaw != 0) > 0]
+verify <- as.matrix(verify[,-c(0,1,2,3)])
+verify <- ngram(verify, window)
+verify.hex  <- as.h2o(verify)
+verify_error <- h2o.anomaly(iot.dl, verify.hex)
+print(nrow(verify_error[verify_error > avg_iot_error])/nrow(verify.hex))
